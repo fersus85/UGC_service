de3c251dfe749ae446c1f091485a500c19c11509
dbacvetkov <dmitry.cvetkov@gmail.com>
2025-01-19 16:11:48 +0000

    scores api

--- src/api/v1/scores.py
+++ src/api/v1/scores.py
@@ -1,9 +1,9 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Path
 from fastapi.params import Depends
-
-from schemas.scores import AddScore
+from uuid import UUID
+from schemas.scores import AddScore, AverageScore
 from services.score_service import FilmScoreService, get_film_score_service
 from utils.helpers import get_user_id_from_access_token
 
@@ -41,3 +41,43 @@ async def add_film_score(
             detail="error when adding a record",
         )
     return None
+
+
+@router.delete(
+    "/{film_id}",
+    status_code=status.HTTP_200_OK,
+    summary="Delete score",
+    description="Удаляет оценку фильма",
+)
+async def delete_film_score(
+    film_id: UUID = Path(
+        title="UUID фильма", example="3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ),
+    user_id: str = Depends(get_user_id_from_access_token),
+    score_service: FilmScoreService = Depends(get_film_score_service),
+) -> None:
+    """Удаляет оценку фильма по его id."""
+    result = await score_service.delete_score(film_id, user_id)
+    if result is None or result == 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="error deleting a record",
+        )
+    return None
+
+
+@router.get("/{film_id}", response_model=AverageScore)
+async def get_film_score(
+    film_id: UUID = Path(
+        title="UUID фильма", example="3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    ),
+    score_service: FilmScoreService = Depends(get_film_score_service),
+) -> AverageScore:
+    """Получает среднюю оценку фильма по его id."""
+    average_score = await score_service.get_score(film_id)
+    if average_score is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Фильм еще никто не оценил",
+        )
+    return AverageScore(average_score=average_score)
--- src/schemas/scores.py
+++ src/schemas/scores.py
@@ -4,5 +4,9 @@ from pydantic import BaseModel, Field
 
 
 class AddScore(BaseModel):
-    film_id: UUID = Field(..., description="UUID фильма")
-    film_score: int = Field(..., description="Оценка", ge=0, le=10)
+    film_id: UUID = Field(..., description="UUID фильма", example="3fa85f64-5717-4562-b3fc-2c963f66afa6")
+    film_score: int = Field(..., description="Оценка", ge=0, le=10, example=10)
+
+
+class AverageScore(BaseModel):
+    average_score: float
