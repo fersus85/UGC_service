47a548ef26269c13ad40c34c931f882a4a3e383a
maxim <maximsonis@gmail.com>
2025-02-02 16:02:18 +0300

    Fix sentry settings

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 
 sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
+    dsn=settings.SENTRY_DSN,
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
